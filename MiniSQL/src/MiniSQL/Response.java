750e0cfb9185a4d34614003973715ffa3a196881
StyxXuan <38460820+StyxXuan@users.noreply.github.com>
2019-05-29 11:08:51 +0800

    src

--- MiniSQL/src/MiniSQL/Response.java
+++ MiniSQL/src/MiniSQL/Response.java
@@ -1,5 +1,15 @@
 package MiniSQL;
 
 public class Response {
-
+	private boolean Affected;
+	private double time;
+	public Response(boolean Aff, double Time) {
+		this.Affected = Aff;
+		this.time = Time;
+	}
+	
+	public void PrintInfor() {
+		System.out.println("the request affected " + this.Affected);
+		System.out.println("using time " + this.time);
+	}
 }
--- MiniSQL/src/MiniTest.java
+++ MiniSQL/src/MiniTest.java
@@ -1,9 +1,41 @@
-import MiniSQL.Interpreter;
 
-//import MiniSQL;
+import java.io.BufferedReader;
+
+import MiniSQL.Interpreter;
+import MiniSQL.Request;
+import MiniSQL.Response;
+
 
 public class MiniTest {
 	public static void main(String[] args){
+		String sql = "";
+		boolean Loop = true;
+		BufferedReader bufferedReader = null;
+		while(Loop) {
+			try {
+				System.out.println("input ");
+				String State = "";
+				State = bufferedReader.readLine();
+				sql += State;
+				if(sql.contains(";")) {
+					boolean check = Interpreter.checkSyntax(sql);
+					if(!check)
+						throw new Exception("SyntaxException");
+					
+					Request req = Interpreter.parse(sql);
+					check = Interpreter.checkLexeme(req);
+					if(!check)
+						throw new Exception("LexemeException");
+					
+					Response resp = Interpreter.excute(req);
+					resp.PrintInfor();
+					sql = "";
+				}
+			}
+			catch(Exception Exp) {
+				System.out.println(Exp.getMessage());
+			}
+		}
 		
 	}
 }
