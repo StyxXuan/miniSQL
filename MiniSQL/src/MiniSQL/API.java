743988b3c7bae4876ef0e82b4fe3f6c74973ff7a
StyxXuan <38460820+StyxXuan@users.noreply.github.com>
2019-06-03 09:24:47 +0800

    src

---
+++ MiniSQL/src/MiniSQL/API.java
@@ -0,0 +1,35 @@
+package MiniSQL;
+
+
+
+public class API{
+	
+	static public Response createTable(Request request) {
+		return null;
+	}
+	
+	static public Response dropTable(Request request) {
+		return null;
+	}
+	
+	static public Response createIndex(Request request) {
+		return null;
+	}
+	
+	static public Response dropIndex(Request request) {
+		return null;
+	}
+	
+	static public Response select(Request request) {
+		return null;
+	}
+	
+	static public Response insert(Request request) {
+		return null;
+	}
+	
+	static public Response delete(Request request) {
+		return null;
+	}
+
+}
