c73bfebd6afbedeb2f0f5e47ecf06953c79058c2
LSXCH <51190956+LSXCH@users.noreply.github.com>
2019-06-04 20:10:27 +0800

    Update Condition.java

--- src/RecordManager/Condition.java
+++ src/RecordManager/Condition.java
@@ -1,5 +1,32 @@
 package RecordManager;
 
-public class Condition {
-
+public class Condition
+{
+	final static int EQUAL = 0; // =
+	final static int NOT_EQUAL = 1; // <>
+	final static int LESS = 2; // <
+	final static int MORE = 3; // >
+	final static int LESS_EQUAL = 4; // <=
+	final static int MORE_EQUAL = 5; // >=
+	
+	boolean Compare_Int()
+	{
+		return false;
+		
+	}
+	boolean Compare_Float()
+	{
+		return false;
+	}
+	boolean Compare_String()
+	{
+		return false;
+	}
+	boolean Compare_Char()
+	{
+		return false;
+	}
+	
+	
+	
 }
